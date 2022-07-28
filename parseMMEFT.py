36d4ce1e86bbfc2a933a384a937e996e6fa76079
Jorge-Alda <jalda@unizar.es>
2022-07-28 11:51:27 +0200

    Working version

--- parseMMEFT.py
+++ parseMMEFT.py
@@ -1,5 +1,5 @@
 import sys
-from typing import List
+from typing import List, Tuple
 from copy import deepcopy
 import re
 
@@ -7,6 +7,70 @@ import re
 privatepars = {'yl', 'ylbar', 'yu', 'yubar', 'yd', 'ydbar', 'g1', 'g2',
                'g3', 'lam', 'muH', 'invepsilonbar', 'KroneckerDelta', 'pi', 'log', 'onelooporder'}
 dummyindices = ['mif1', 'mif2', 'mif3', 'mif4', 'mif5', 'MIF1']
+
+noindices = {'alphaO3G': 'G',
+             'alphaO3Gt': 'Gtilde',
+             'alphaO3W': 'W',
+             'alphaO3Wtilde': 'Wtilde',
+             'alphaOH': 'phi',
+             'alphaOHBox': 'phiBox',
+             'alphaOHD': 'phiD',
+             'alphaOHG': 'phiG',
+             'alphaOHB': 'phiB',
+             'alphaOHW': 'phiW',
+             'alphaOHWB': 'phiWB',
+             'alphaOHGt': 'phiGtilde',
+             'alphaOHBt': 'phiBtilde',
+             'alphaOHWt': 'phiWtilde',
+             'alphaOHWBt': 'phiWtildeB'}
+indices33 = {'alphaOuH': 'uphi',
+             'alphaOdH': 'dphi',
+             'alphaOeH': 'ephi',
+             'alphaOeW': 'eW',
+             'alphaOeB': 'eB',
+             'alphaOuG': 'uG',
+             'alphaOuW': 'uW',
+             'alphaOuB': 'uB',
+             'alphaOdG': 'dG',
+             'alphaOdW': 'dW',
+             'alphaOdB': 'dB',
+             'alphaOHud': 'phiud'}
+indices3t = {'alphaOHl1': 'phil1',
+             'alphaOHl3': 'phil3',
+             'alphaOHe': 'phie',
+             'alphaOHq1': 'phiq1',
+             'alphaOHq3': 'phiq3',
+             'alphaOHu': 'phiu',
+             'alphaOHd': 'phid'}
+indices4fa = {'alphaOll': 'll',
+              'alphaOqq1': 'qq1',
+              'alphaOqq3': 'qq3',
+              'alphaOuu': 'uu',
+              'alphaOdd': 'dd'}
+indices4fb = {'alphaOlq1': 'lq1',
+              'alphaOlq3': 'lq3',
+              'alphaOeu': 'eu',
+              'alphaOed': 'ed',
+              'alphaOud1': 'ud1',
+              'alphaOud8': 'ud8',
+              'alphaOle': 'le',
+              'alphaOlu': 'lu',
+              'alphaOld': 'ld',
+              'alphaOqe': 'qe',
+              'alphaOqu1': 'qu1',
+              'alphaOqu8': 'qu8',
+              'alphaOqd1': 'qd1',
+              'alphaOqd8': 'qd8',
+              'alphaOledq': 'ledq',
+              'alphaOquqd1': 'quqd1',
+              'alphaOquqd8': 'quqd8',
+              'alphaOlequ1': 'lequ1',
+              'alphaOlequ3': 'lequ3'}
+
+allwcs = list(noindices.keys()) + list(indices33.keys()) + \
+    list(indices3t.keys()) + list(indices4fa.keys()) + list(indices4fb.keys())
+allwcs.append('alphaOee')
+
 
 def curlying(text: str) -> List[str]:
     curly = 0
@@ -65,124 +129,20 @@ def splitcoeff(text: str) -> List[str]:
     return ll
 
 
-def to_wcxf(MMEFTcoeffs: dict) -> dict:
-    result = {}
-    noindices = {'alphaO3G': 'G',
-                 'alphaO3Gt': 'Gtilde',
-                 'alphaO3W': 'W',
-                 'alphaO3Wtilde': 'Wtilde',
-                 'alphaOH': 'phi',
-                 'alphaOHBox': 'phiBox',
-                 'alphaOHD': 'phiD',
-                 'alphaOHG': 'phiG',
-                 'alphaOHB': 'phiB',
-                 'alphaOHW': 'phiW',
-                 'alphaOHWB': 'phiWB',
-                 'alphaOHGt': 'phiGtilde',
-                 'alphaOHBt': 'phiBtilde',
-                 'alphaOHWt': 'phiWtilde',
-                 'alphaOHWBt': 'phiWtildeB'}
-    indices33 = {'alphaOuH': 'uphi',
-                 'alphaOdH': 'dphi',
-                 'alphaOeH': 'ephi',
-                 'alphaOeW': 'eW',
-                 'alphaOeB': 'eB',
-                 'alphaOuG': 'uG',
-                 'alphaOuW': 'uW',
-                 'alphaOuB': 'uB',
-                 'alphaOdG': 'dG',
-                 'alphaOdW': 'dW',
-                 'alphaOdB': 'dB',
-                 'alphaOHud': 'phiud'}
-    indices3t = {'alphaOHl1': 'phil1',
-                 'alphaOHl3': 'phil3',
-                 'alphaOHe': 'phie',
-                 'alphaOHq1': 'phiq1',
-                 'alphaOHq3': 'phiq3',
-                 'alphaOHu': 'phiu',
-                 'alphaOHd': 'phid'}
-    indices4fa = {'alphaOll': 'll',
-                  'alphaOqq1': 'qq1',
-                  'alphaOqq3': 'qq3',
-                  'alphaOuu': 'uu',
-                  'alphaOdd': 'dd'}
-    indices4fb = {'alphaOlq1': 'lq1',
-                  'alphaOlq3': 'lq3',
-                  'alphaOeu': 'eu',
-                  'alphaOed': 'ed',
-                  'alphaOud1': 'ud1',
-                  'alphaOud8': 'ud8',
-                  'alphaOle': 'le',
-                  'alphaOlu': 'lu',
-                  'alphaOld': 'ld',
-                  'alphaOqe': 'qe',
-                  'alphaOqu1': 'qu1',
-                  'alphaOqu8': 'qu8',
-                  'alphaOqd1': 'qd1',
-                  'alphaOqd8': 'qd8',
-                  'alphaOledq': 'ledq',
-                  'alphaOquqd1': 'quqd1',
-                  'alphaOquqd8': 'quqd8',
-                  'alphaOlequ1': 'lequ1',
-                  'alphaOlequ3': 'lequ3'}
-    for coeff, values in MMEFTcoeffs.items():
-        indx: List[str] = values[0]
-        formula: str = values[1]
-        if coeff in noindices.keys():
-            result.update({noindices[coeff]: formula})
-        if coeff in indices33.keys():
-            for i in range(3):
-                for j in range(3):
-                    coeffname = f'{indices33[coeff]}_{i+1}{j+1}'
-                    formflavour = formula.replace(indx[0], str(i))
-                    formflavour = formflavour.replace(indx[1], str(j))
-                    result.update({coeffname: formflavour})
-        if coeff in indices3t.keys():
-            for i in range(3):
-                for j in range(i, 3):
-                    coeffname = f'{indices3t[coeff]}_{i+1}{j+1}'
-                    formflavour = formula.replace(indx[0], str(i))
-                    formflavour = formflavour.replace(indx[1], str(j))
-                    result.update({coeffname: formflavour})
-        if coeff in indices4fa:
-            for label in ['1111', '1112', '1113', '1122', '1123', '1133', '1122', '1123', '1133', '1212', '1213', '1221', '1222', '1223', '1231', '1232', '1233', '1313', '1322', '1323', '1331', '1332', '1333', '2222', '2223', '2233', '2323', '2333', '3333']:
-                i = int(label[0])
-                j = int(label[1])
-                k = int(label[2])
-                l = int(label[3])
-                coeffname = f'{indices4fa[coeff]}_{i}{j}{k}{l}'
-                formflavour = formula.replace(indx[0], str(i-1))
-                formflavour = formflavour.replace(indx[1], str(j-1))
-                formflavour = formflavour.replace(indx[2], str(k-1))
-                formflavour = formflavour.replace(indx[3], str(l-1))
-                result.update({coeffname: formflavour})
-        if coeff in indices4fb:
-            for i in range(3):
-                for j in range(i, 3):
-                    for k in range(3):
-                        for l in range(k, 3):
-                            coeffname = f'{indices4fb[coeff]}_{i+1}{j+1}{k+1}{l+1}'
-                            formflavour = formula.replace(indx[0], str(i))
-                            formflavour = formflavour.replace(indx[1], str(j))
-                            formflavour = formflavour.replace(indx[2], str(k))
-                            formflavour = formflavour.replace(indx[3], str(l))
-                            result.update({coeffname: formflavour})
-        if coeff == 'alphaOee':
-            for i in range(3):
-                for j in range(i, 3):
-                    for k in range(3):
-                        for l in range(k, 3):
-                            if int(k+l) >= int(i+j) and i+j+k+l != '1322':
-                                coeffname = f'ee_{i+1}{j+1}{k+1}{l+1}'
-                                formflavour = formula.replace(indx[0], str(i))
-                                formflavour = formflavour.replace(
-                                    indx[1], str(j))
-                                formflavour = formflavour.replace(
-                                    indx[2], str(k))
-                                formflavour = formflavour.replace(
-                                    indx[3], str(l))
-                                result.update({coeffname: formflavour})
-    return result
+def extractpars(formula: str) -> Tuple[List[str], List[str]]:
+    parameters = set(re.findall(r'[a-zA-Z]+[0-9a-zA-Z]*', formula))
+    parameters -= privatepars
+    parameters -= set(dummyindices)
+    barpars = []
+    for p in parameters:
+        if p[-3:] == 'bar':
+            barpars.append(p)
+    parameters -= set(barpars)
+    parameters = list(parameters)
+    parameters.sort()
+    barpars.sort()
+    return (parameters, barpars)
+
 
 if __name__ == '__main__':
     inputfile = sys.argv[1]
@@ -203,7 +163,6 @@ if __name__ == '__main__':
 
     items = splitcoeff(rules)
     dicitems = {}
-    parameters = set()
     for it in items:
         sp = it.split('->')
         head = sp[0]
@@ -213,38 +172,130 @@ if __name__ == '__main__':
         formula = formula.replace('/', ' / ')
         formula = formula.replace('^', '**')
         if formula != '0':
-            pars = re.findall(r'[a-zA-Z]+[0-9a-zA-Z]*', formula)
-            parameters |= set(pars)
             if '[' in head:
                 (coeff, indx) = head.split('[')
                 indx = indx.replace(']', '')
-                dicitems.update({coeff: (indx.split(','), formula)})
+                if coeff in allwcs:
+                    dicitems.update({coeff: (indx.split(','), formula)})
             else:
-                dicitems.update({head: (None, formula)})
-    wcxfcoeffs = to_wcxf(dicitems)
-    parameters -= privatepars
-    parameters -= set(dummyindices)
-    barpars = []
-    for p in parameters:
-        if p[-3:] == 'bar':
-            barpars.append(p)
-    parameters -= set(barpars)
-    parameters = list(parameters)
-    parameters.sort()
+                if head in allwcs:
+                    dicitems.update({head: (None, formula)})
 
     with open(outputfile, 'wt') as f:
-        f.write(
-            'from numpy import pi, log, conj\nfrom sminputs import g1, g2, g3, lam, yu, yd, yl\n\nKroneckerDelta = lambda x, y: int(x==y)\n\ninvepsilonbar=0\nonelooporder=1\n\n')
+        f.write('from numpy import pi, log, conj\n')
+        f.write('import sminputs\n\n')
+        for p in ['g1', 'g2', 'g3', 'lam', 'yu', 'yd', 'yl']:
+            f.write(f'{p} = sminputs.parameters["{p}"]\n')
+        f.write('yubar = conj(yu)\n')
+        f.write('ydbar = conj(yd)\n')
+        f.write('ylbar = conj(yl)\n\n')
+        f.write('KroneckerDelta = lambda x, y: int(x==y)\n\n')
+        f.write('invepsilonbar=0\nonelooporder=1\n\n')
+
+        f.write('def reloadsminputs():\n')
+        for p in ['g1', 'g2', 'g3', 'lam', 'yu', 'yd', 'yl']:
+            f.write(f'\tglobal {p}\n\t{p} = sminputs.parameters["{p}"]\n')
+        f.write('\tglobal yubar \n\tyubar = conj(yu)\n')
+        f.write('\tglobal ydbar \n\tydbar = conj(yd)\n')
+        f.write('\tglobal ylbar \n\tylbar = conj(yl)\n\n')
+
+        wcdict = ""
+        parameters = set()
+
+        for coeff, values in dicitems.items():
+            f.write(f'def {coeff}(')
+            indx: List[str] = values[0]
+            formula: str = values[1]
+            pars, bpars = extractpars(formula)
+            pars = set(pars)
+            if indx is not None:
+                pars -= set(indx)
+                for i in range(len(indx)):
+                    f.write(f'flavour{i}, ')
+                    formula = formula.replace(indx[i], f'flavour{i}')
+            parameters |= pars
+
+            for p in pars:
+                f.write(f'{p}, ')
+
+            f.write('):\n')
+            for bp in bpars:
+                f.write(f'\t{bp} = conj({bp[:-3]})\n')
+            dummies = []
+            for d in dummyindices:
+                if d in formula:
+                    dummies.append(d)
+            if len(dummies) == 0:
+                f.write('\treturn ' + formula)
+            else:
+                f.write('\tresult = 0\n')
+                for i, d in enumerate(dummies):
+                    f.write('\t'*(i+1))
+                    f.write(f'for {d} in range(3):\n')
+                f.write('\t'*(len(dummies)+2))
+                f.write('result += ' + formula + '\n')
+                f.write('\treturn result')
+
+            if coeff in noindices.keys():
+                coeffname = noindices[coeff]
+                wcdict += f'\t        "{coeffname}": {coeff}('
+                for p in pars:
+                    wcdict += f'{p}, '
+                wcdict += '),\n'
+            if coeff in indices33.keys():
+                for i in range(3):
+                    for j in range(3):
+                        coeffname = f'{indices33[coeff]}_{i+1}{j+1}'
+                        wcdict += f'\t        "{coeffname}": {coeff}({i}, {j}, '
+                        for p in pars:
+                            wcdict += f'{p}, '
+                        wcdict += '),\n'
+            if coeff in indices3t.keys():
+                for i in range(3):
+                    for j in range(i, 3):
+                        coeffname = f'{indices3t[coeff]}_{i+1}{j+1}'
+                        wcdict += f'\t        "{coeffname}": {coeff}({i}, {j}, '
+                        for p in pars:
+                            wcdict += f'{p}, '
+                        wcdict += '),\n'
+            if coeff in indices4fa:
+                for label in ['1111', '1112', '1113', '1122', '1123', '1133', '1122', '1123', '1133', '1212', '1213', '1221', '1222', '1223', '1231', '1232', '1233', '1313', '1322', '1323', '1331', '1332', '1333', '2222', '2223', '2233', '2323', '2333', '3333']:
+                    i = int(label[0])
+                    j = int(label[1])
+                    k = int(label[2])
+                    l = int(label[3])
+                    coeffname = f'{indices4fa[coeff]}_{i}{j}{k}{l}'
+                    wcdict += f'\t        "{coeffname}": {coeff}({i-1}, {j-1}, {k-1}, {l-1}, '
+                    for p in pars:
+                        wcdict += f'{p}, '
+                    wcdict += '),\n'
+            if coeff in indices4fb:
+                for i in range(3):
+                    for j in range(i, 3):
+                        for k in range(3):
+                            for l in range(k, 3):
+                                coeffname = f'{indices4fb[coeff]}_{i+1}{j+1}{k+1}{l+1}'
+                                wcdict += f'\t        "{coeffname}": {coeff}({i}, {j}, {k}, {l}, '
+                                for p in pars:
+                                    wcdict += f'{p}, '
+                                wcdict += '),\n'
+            if coeff == 'alphaOee':
+                for label in ['1111', '1112', '1113', '1122', '1123', '1133', '1212', '1213', '1222', '1223', '1232', '1233', '1313', '1323', '1333', '2222', '2223', '2233', '2323', '2333', '3333']:
+                    i = int(label[0])
+                    j = int(label[1])
+                    k = int(label[2])
+                    l = int(label[3])
+                    coeffname = f'ee_{i}{j}{k}{l}'
+                    wcdict += f'\t        "{coeffname}": {coeff}({i-1}, {j-1}, {k-1}, {l-1}, '
+                    for p in pars:
+                        wcdict += f'{p}, '
+                    wcdict += '),\n'
+            f.write('\n\n')
+
         f.write('def wc(')
         for p in parameters:
-            default = 0
-            if p == 'HIGHSCALE':
-                default = 1000
-            f.write(f'{p}={default}, ')
+            f.write(f'{p}, ')
         f.write('):\n')
-        for bp in barpars:
-            f.write(f'\t{bp} = conj({bp[:-3]})\n')
-        f.write('\treturn {\n')
-        for k, v in wcxfcoeffs.items():
-            f.write('\t        "' + k + '": ' + v + ',\n')
+
+        f.write('\treturn {\n' + wcdict)
         f.write('\t}')
